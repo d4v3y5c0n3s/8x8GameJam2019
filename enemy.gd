adb6c06c412010c887a19bc82677cdc076c846d2
Troy Jacobs <tmj9001@gmail.com>
2019-07-03 22:21:15 -0400

    character movement is FIXED!!!!  among other changes...

--- enemy.gd
+++ enemy.gd
@@ -6,46 +6,44 @@ onready var tween_node = $twn#  reference to the tween node
 
 var direction = 1#  used to determine movement, 0 = left, 1 = up, 2 = right, 3 = down
 var abilities = 0#  0 = no abilities, 1 = purple disguise, 2 = green disguise, 3 = body bag
-var alive = true#  used to control logic related to an agent getting killed
+var alive = true#  used to control logic related to an enemy getting killed
 var at_point = false#  used to trigger logic related to to arriving at points
 var starting_point = Vector2()#  this and end_point are used to move and determine
-var end_point = Vector2()#  when the agent needs to check if it should change direction
+var end_point = Vector2()#  when the enemy needs to check if it should change direction
 var finished_spawn = false
-#var previous_end_point = Vector2()
+var previous_end_point = Vector2()
 
 func _ready():
 	set_process(false)
 	$twn.connect("tween_completed", self, "twn_finished")
 
-#  used for finding an initial point when the agent is placed
-func _on_agent_area_entered(area):
+#  used for finding an initial point when the enemy is placed
+func _on_enemy_area_entered(area):
 	if not finished_spawn and area.get_parent().get_name() == "grid":
 		at_point = true
 		finished_spawn = true
 		self.set_position(area.get_position())
 		set_process(true)
 
-#  used to ensure that the tween brings the agent to exactly where it need to be
+#  used to ensure that the tween brings the enemy to exactly where it need to be
 func twn_finished(obj, np_key):
 	set_position(end_point)
 	at_point = true
-	tween_node.stop_all()
-
+	
 func move(target_point, output=null): # sets end_point for the agents (used in _process)
 	if output != null:
-		#print(output)
-		pass
+		print(output)
 	end_point = target_point.get_position()
 	starting_point = self.get_position()
 	at_point = false
 
-func _physics_process(delta):
+func _process(delta):
 	#  for debugging purposes
-#	if previous_end_point != end_point:
-#		previous_end_point = end_point
-#		#print(end_point)
+	if previous_end_point != end_point:
+		previous_end_point = end_point
+		print(end_point)
 	match at_point:
-		true:#  agent is at point, start moving towards another point
+		true:#  enemy is at point, start moving towards another point
 			for n in get_parent().get_parent().get_children():
 				if n.get_name() == "grid":#  gets the level's grid node
 					var nearby = n.check_nearby(self.get_position())#  get the 8 surrounding points from the grid (by calling check_nearby(point))
@@ -127,6 +125,6 @@ func _physics_process(delta):
 								direction = 1
 								move(nearby[1], "going top center")
 							break
-		false:#  agent hasn't arrived at a point yet, continue moving towards it
+		false:#  enemy hasn't arrived at a point yet, continue moving towards it
 			tween_node.interpolate_property(self, "position", starting_point, end_point, WALK_SPEED, Tween.TRANS_LINEAR, Tween.EASE_IN)
 			tween_node.start()
